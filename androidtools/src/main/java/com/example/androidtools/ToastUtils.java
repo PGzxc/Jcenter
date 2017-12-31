0742c270681688152f4ebc4da155b3d1a7579f9c
pgzxc <827489398@qq.com>
2017-12-31 11:20:13 +0800

    use jcenter resource

--- androidtools/src/main/java/com/example/androidtools/ToastUtils.java
+++ androidtools/src/main/java/com/example/androidtools/ToastUtils.java
@@ -12,23 +12,19 @@ public class ToastUtils {
     private static final String TAG = "ToastUtil";
 
 
-    //短时间吐司
     public static void show(Context context, int resourceID) {
         show(context, resourceID, Toast.LENGTH_SHORT);
     }
 
-    //短时间吐司
     public static void show(Context context, String text) {
         show(context, text, Toast.LENGTH_SHORT);
     }
 
-    //自定义时长吐司
     public static void show(Context context, Integer resourceID, int duration) {
-        String text = context.getResources().getString(resourceID);// 用于显示的文字
+        String text = context.getResources().getString(resourceID);
         show(context, text, duration);
     }
 
-    //自定义时长吐司
     public static void show(final Context context, final String text, final int duration) {
         if (TOAST == null) {
             TOAST = Toast.makeText(context, text, duration);
--- app/src/main/java/com/example/jcenter/MainActivity.java
+++ app/src/main/java/com/example/jcenter/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.jcenter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.androidtools.ToastUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+     @BindView(R.id.textView)
+     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        ToastUtils.show(this,"吐司");
     }
 }
